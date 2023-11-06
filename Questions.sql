3f002072b8a2b47de379f0fa0ebee6715e5ecb68
chethanavirajini <cheth.virajini@gmail.com>
2023-11-06 23:14:30 +0530

    question2

--- Questions.sql
+++ Questions.sql
@@ -2,3 +2,8 @@
 
 SELECT Person.firstname, Person.lastname, Address.city, Address.state FROM Person INNER JOIN Address ON Person.personID = Address.personID;
 
+2. Get second higest salary in salaray table
+
+SELECT MAX(amount) AS second_highest_salary
+FROM salary
+WHERE amount < (SELECT MAX(amount) FROM salary);
